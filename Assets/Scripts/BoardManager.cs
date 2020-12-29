383555dce1676d46285d293536c5a9eb3098e9dd
Carlos <cgm164@alu.ua.es>
2020-12-29 17:37:37 +0100

    Proyecto

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public GameObject cell;
+    public GameObject pawn;
+    public GameObject horse;
+    public GameObject rook;
+    public GameObject sharp;
+    public GameObject queen;
+    public GameObject king;
+
+    Ray ray;
+    RaycastHit hit;
+
+    private GameObject pieceSelect;
+    private GameObject cellSelect;
+
+    private enum Status
+    {
+        SELECT_PIECE, 
+        SELECT_CELL, 
+        MOVE_PIECE
+    };
+
+    private Status status;
+
+    private void GeneratePiece(GameObject obj, GameObject piece, float height, float rotation)
+    {
+        GameObject iPiece = Instantiate(piece);
+        float x = obj.transform.position.x;
+        float y = obj.transform.position.y;
+        float z = obj.transform.position.z;
+
+        iPiece.transform.position += new Vector3(x, y + height, z);
+        iPiece.transform.rotation  = Quaternion.Euler(0, rotation, 0);
+    }
+
+    private void GenerateBoard()
+    {
+        float size = cell.GetComponent<Collider>().bounds.size.x;
+        float height = cell.GetComponent<Collider>().bounds.size.y;
+
+        for (int i = 0; i < 8; i++)
+            for (int j = 0; j < 8; j++) {
+                GameObject obj = Instantiate(cell);
+                obj.transform.position += new Vector3(size * i, 0,  size * j);
+
+                if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0)
+                    obj.GetComponent<MeshRenderer>().material.color = Color.black;
+                else
+                    obj.GetComponent<MeshRenderer>().material.color = Color.white;
+
+                obj.name = char.ConvertFromUtf32(i + 97) + "" + (j + 1);
+
+                if (j == 1 || j == 6)
+                {
+                    GeneratePiece(obj, pawn, height, 0);
+
+                } else if (j == 0 || j == 7)
+                {
+                    float rotation = j == 0 ? 0 : -180;
+
+                    if (i == 0 || i == 7)
+                        GeneratePiece(obj, rook, height, rotation);
+                    if (i == 1 || i == 6)
+                        GeneratePiece(obj, horse, height, rotation);
+                    if (i == 2 || i == 5)
+                        GeneratePiece(obj, sharp, height, rotation);
+                    if (i == 3)
+                        GeneratePiece(obj, queen, height, rotation);
+                    else 
+                        GeneratePiece(obj, king, height, rotation);
+                }
+            }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateBoard();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                //print(hit.collider.name);
+                MeshRenderer render = hit.collider.GetComponent<MeshRenderer>();
+
+                if (render != null && status == Status.SELECT_PIECE)
+                {
+                    status = Status.SELECT_CELL;
+                    pieceSelect = hit.collider.gameObject;
+                    render.material.color = Color.red;
+
+                } else if (render == null && status == Status.SELECT_CELL)
+                {
+                    status = Status.MOVE_PIECE;
+                    cellSelect = hit.collider.gameObject;
+                    
+
+
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Chess.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class Chess : MonoBehaviour
+{
+    private string status = "";
+    public List<GameObject> pieces;
+
+    public Chess() { }
+    
+    IEnumerator Request(string url, Action<JSONNode> callback)
+    {
+        var request = new UnityWebRequest(url, "GET")
+        {
+            downloadHandler = new DownloadHandlerBuffer()
+        };
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        
+        callback(JSONNode.Parse(request.downloadHandler.text));
+        
+    }
+
+    public void Move(string movement)
+    {
+        string nextStatus = status + movement;
+
+        StartCoroutine(Request($"http://chess-api.herokuapp.com/valid_move/{nextStatus}", result =>
+        {
+            if (result["validMove"])
+                status = nextStatus;
+            Debug.Log(status);
+        }));
+    }
+
+    public void Move()
+    {
+        StartCoroutine(Request($"http://chess-api.herokuapp.com/next_best/{status}", result =>
+        {
+            status += result["bestNext"];
+            Debug.Log(status);
+        }));
+    }
+
+    public void Start()
+    {
+        status = "a2a4";
+        Move("a2a10");
+        Move();
+    }
+
+}
