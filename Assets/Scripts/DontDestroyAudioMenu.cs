4f2796a427e09e88b34aacea9fd0a50d22a2a623
Sergio Cortés <sce17@alu.ua.es>
2021-01-14 20:17:58 +0100

    Slider (Not functional)

---
+++ Assets/Scripts/DontDestroyAudioMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DontDestroyAudioMenu : MonoBehaviour
+{
+    public static DontDestroyAudioMenu instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
